a07ee9e9620392a00781b7ed55e2da3b351ebcab
yon yen <yon.yen@pnc.lan>
2019-08-26 20:23:40 +0700

    operator

---
+++ js/practice4.js
@@ -0,0 +1,15 @@
+function sumNumber(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let sum = 0;
+    sum = Number(num1)+Number(num2);
+    alert(sum);
+}
+
+function minusNumber(){
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let minus = 0;
+    minus = Number(num1)-Number(num2);
+    alert(minus);
+}
